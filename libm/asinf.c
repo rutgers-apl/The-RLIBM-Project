a09a2964d638feba6ea735c328fb6315e41632ec
Santosh Nagarakatte <sn349@skylake.cs.rutgers.edu>
2022-11-21 12:42:22 -0500

    adding a new fma version of asinf. Slightly faster than the older version

--- libm/asinf.c
+++ libm/asinf.c
@@ -25,82 +25,79 @@ SOFTWARE.
 */
 
 
-#include <math.h>
-#include "pi.h"
 #include "rlibm.h"
-
-#define TRUE 1
-#define FALSE 0
-
+#include "sinpicospi.h"
+#include "pi.h"
+#include <math.h>
+#include <stdbool.h>
 
 #define PI_2 0x1.921fb54442d18p+0
 
 double rlibm_asinf(float x) {
-  float_x fx;
-  fx.f = x;
-  int s = (fx.x & 0x80000000) ? -1 : 1;
-  fx.x &= 0x7FFFFFFF;
-  if (fx.x > 0x3F800000) {
+  double_x dY;
+  float_x fX;
+  fX.f = x;
+  fX.x &= 0x7FFFFFFF;
+  uint64_t sgn = (x < 0) ? 0x8000000000000000 : 0; 
+  if (fX.x > 0x3F800000) {
     return 0.0f/0.0f;
   }
-  if (fx.x < 0x39E89768) {
-    return 0x1.0000008000007p+0 * fx.f * s;
-  }
-  if (fx.x == 0x3AE3A41D) {
-    return 0x1.c748488p-10 * s;
-  } 
-  if (fx.x == 0x3AF64DCF) {
-    return 0x1.ec9bb08p-10 * s;
-  }
-  if (fx.x == 0x3AFA8D28) {
-    return 0x1.f51a638p-10 * s;
-  }
-  if (fx.x == 0x3AFEA8D6) {
-    return 0x1.fd51c08p-10 * s;
-  }
-  if (fx.x == 0x3B5637DC) {
-    return 0x1.ac6fe98p-9 * s;
-  }
-  if (fx.x == 0x3B7A8D0D) {
-    return 0x1.f51a698p-9 * s;
-  }
-  if (fx.x == 0x3F083A1A) {
-    return 0x1.1f4b648p-1 * s;
-  }
-  double y;
-  double R = fx.f;
-  R /= sqrt(1.0 - R*R);
-  int reciprocal = FALSE;
-  if (R > 1.0) {
-    R = 1/R;
-    reciprocal = TRUE;
-  }
-  double atan_b = 0.0;
-  if (R > 0.001953125) {
-    int r;
-    {
-      double_x dx;
-      dx.d = R;
-      dx.x -= 1;
-      uint32_t value = 0x80 | ((dx.x >> 45) & 0x7f);
-      int exponent = dx.x >> 52;
-      r = value >> (8 - (exponent - 0x3f6));
+  double R, R2;
+  if (fX.x < 0x39E89768) {
+    dY.d = 0x1.0000008000007p+0*fX.f;
+    dY.x ^= sgn;
+    return dY.d;
+  } else if (fX.x <= 0x3B000000) {
+    R = fX.f;
+    R2 = R*R;
+    double temp = fma(R2, 0x1.40076be912c25p-4, 0x1.5555534e4e023p-3);
+    dY.d = R*fma(temp, R2, 0x1.000000000003fp+0);
+    dY.x ^= sgn;
+    return dY.d;
+  } else {
+    if (fX.x == 0x3B5637DC) {
+      dY.d = 0x1.ac6fe98p-9;
+      dY.x ^= sgn;
+      return dY.d;
     }
-    double b = r*0.00390625 + 0.001953125;
-    R = (R - b)/(1.0L + b*R);
-    atan_b = atan_vals[r];
+    if (fX.x == 0x3B7A8D0D) {
+      dY.d = 0x1.f51a698p-9;
+      dY.x ^= sgn;
+      return dY.d;
+    }
+    if (fX.x == 0x3F083A1A) {
+      dY.d = 0x1.1f4b648p-1;
+      dY.x ^= sgn;
+      return dY.d;
+    }
+    R = fX.f;
+    R /= sqrt(1.0 - R*R);
+    bool reciprocal = false;
+    if (R > 1.0) {
+      R = 1/R;
+      reciprocal = true;
+    }
+    double atan_b = 0.0;
+    if (R > 0.001953125) {
+      int r;
+      double_x dX;
+      dX.d = R;
+      dX.x -= 1;
+      uint32_t value = 0x80 | ((dX.x >> 45) & 0x7f);
+      int exponent = dX.x >> 52;
+      r = value >> (8 - (exponent - 0x3f6));
+      double b = fma(r, 0.00390625, 0.001953125);
+      R = (R - b)/fma(R, b, 1.0L);
+      atan_b = atan_vals[r];
+    }
+    double R2 = R*R, temp1, temp2;
+    temp1 = fma(R2, 0x1.9e14ca1a8790bp-3, -0x1.55555591c2c0ap-2);
+    temp2 = fma(temp1, R2, 0x1.0000000000005p+0);
+    dY.d = fma(temp2, R, atan_b);
+    if (reciprocal) {
+      dY.d = PI_2 - dY.d;
+    }
   }
-  double R2 = R*R;
-  y = 0x1.9e14ca1a8790bp-3;
-  y *= R2;
-  y += -0x1.55555591c2c0ap-2;
-  y *= R2;
-  y += 0x1.0000000000005p+0;
-  y *= R;
-  y += atan_b;
-  if (reciprocal) {
-    y = PI_2 - y;
-  }
-  y *= s;
-  return y;
+  dY.x ^= sgn;
+  return dY.d;
 }
