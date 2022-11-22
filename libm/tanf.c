e90633dd2f24bd8bb4bd53372ec2940f29481901
Santosh Nagarakatte <sn349@skylake.cs.rutgers.edu>
2022-11-21 19:18:37 -0500

    New version of correctly rounded tanf. 26% faster than the previous version

--- libm/tanf.c
+++ libm/tanf.c
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2022 Sehyeok Park and Santosh Nagarakatte, Rutgers
+Copyright (c) 2022 Sehyeok Park, Mridul Aanjaneya, and Santosh Nagarakatte, Rutgers
 Architecture and Programming Languages (RAPL) Group
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -24,81 +24,93 @@ SOFTWARE.
 
 */
 
-
-#include <math.h>
-#include "pi.h"
 #include "rlibm.h"
 #include "sinpicospi.h"
+#include "pi.h"
+#include <math.h>
 
-#define PI_OVER_512 0x1.921fb54442d18p-8 
 
-double rlibm_tanf(float x) {
-  float_x fx;
-  fx.f = x; 
-  int s = (fx.x & 0x80000000) ? -1 : 1;
-  fx.x &= 0x7FFFFFFF;
-  if (fx.x >= 0x7F800000) {
-    return 0.0f/0.0f;
-  }
-  if (fx.x == 0x3F8A1F62) {
-    return 0x1.ddf9f58p+0*s;
-  }
-  if (fx.x == 0x4D56D355) {
-    return 0x1.e803048p-3*s;
-  }
-  if (fx.x == 0x5FFD33A4) {
-    return 0x1.a0d9178p+0*s;
-  }
-  if (fx.x == 0x6AD36709) {
-    return -0x1.c5612e8p-1*s;
-  }
-  double y;
-  if (fx.x < 0x39B89BA3) {
-    y = fx.f * 0x1.0000008000007p+0*s;
-  } else {   
-    double R;
-    int64_t N;
+#define PI_OVER_512 0x1.921fb54442d18p-8
 
-    if (fx.x <= 0x48000000) {
-      double prod = fx.f * _512_over_pi_28[0];
-      double kd = floor(prod);
-      N = (int64_t) kd;
-      R = prod - kd;
-      R = fma(fx.f, _512_over_pi_28[1], R);
-      R = fma(fx.f, _512_over_pi_28[2], R);
+typedef unsigned __int128 uint128_t;
+
+
+double rlibm_tanf(float x) { 
+  double_x dY;
+  float_x fX;
+  fX.f = x;
+  fX.x &= 0x7FFFFFFF;
+  uint64_t sgn = (x < 0) ? 0x8000000000000000 : 0;
+  double R, R2;
+  if (fX.x < 0x39B89BA3) {
+    dY.d = fX.f*0x1.0000008000007p+0;
+    dY.x |= sgn;
+    return dY.d;
+  } else if (fX.x < 0x7F800000) {   
+    int N;
+    if (fX.x < 0x49800000) {
+      if (fX.x < 0x3C490FDB) {
+	R = fX.f;
+	double R2 = R*R;
+	double temp = fma(R2, 0x1.1117b1df3ecb4p-3, 0x1.55555549f55f8p-2);
+	dY.d = R*fma(temp, R2, 0x1.0000000000003p+0);
+	dY.x |= sgn;
+	return dY.d;
+      }
+      if (fX.x == 0x3F8A1F62) {
+	dY.d = 0x1.ddf9f58p+0;
+	dY.x ^= sgn;
+	return dY.d;
+      }
+      R = 0x1.45f306cp+7 * fX.f;
+      N = R;
+      R -= N;
+      R = fma(0x1.c9c882a53f84fp-21, fX.f, R);
       if (R > 1.0f) {
 	N += 1;
 	R -= 1.0f;
       }
     } else {
-      int x_biased_e = fx.x >> 23L;
-      int x_lsb_e = x_biased_e - 150;
-      int idx = 0;
-      while (x_lsb_e + _512_over_pi_28_exp[idx] > 0) {
-	idx++;
+      if (fX.x == 0x4D56D355) {
+	dY.d = 0x1.e803048p-3;
+	dY.x ^= sgn;
+	return dY.d;
       }
-      double prod_hi =  (idx) ? fx.f * _512_over_pi_28[idx-1] : 0;
-      int64_t k_hi_int = (int64_t) prod_hi;
-      k_hi_int &= 0x3FF;
-      double k_hi = (double) k_hi_int;
-      double prod_mid = fx.f * _512_over_pi_28[idx];
-      double k_mid = floor(prod_mid);
-      double frac = prod_mid - k_mid;
-      double prod_lo = fma(fx.f, _512_over_pi_28[idx + 1], frac);
-      double k_lo = floor(prod_lo); 
-      N = (int64_t)(k_hi + k_mid + k_lo);
-
-      R = prod_lo - k_lo;
-      R = fma(fx.f, _512_over_pi_28[idx + 2], R);
-      R = fma(fx.f, _512_over_pi_28[idx + 3], R); 
-      if (R > 1.0f) {
-	N += 1;
-	R -= 1.0f;
+      if (fX.x == 0x5FFD33A4) {
+        dY.d = 0x1.a0d9178p+0;
+	dY.x ^= sgn;
+	return dY.d;
       }
+      if (fX.x == 0x6AD36709) {
+	dY.d = -0x1.c5612e8p-1;
+	dY.x ^= sgn;
+	return dY.d;
+      }  
+      int e = (fX.x >> 23) - 127;
+      int s = e - 23;
+      uint64_t m = (fX.x&0x7FFFFF)|1<<23;
+      static const uint64_t ipi[] = {0xfe5163abdebbc562, 0xdb6295993c439041, 0xfc2757d1f534ddc0, 0xa2f9836e4e441529};
+      uint128_t p0 = (uint128_t)m*ipi[0];
+      uint128_t p1 = (uint128_t)m*ipi[1]; p1 += p0>>64;
+      uint128_t p2 = (uint128_t)m*ipi[2]; p2 += p1>>64;
+      uint128_t p3 = (uint128_t)m*ipi[3]; p3 += p2>>64;
+      uint64_t p3h = p3>>64, p3l = p3, p2l = p2, p1l = p1;
+      uint64_t a;
+      if (s < 56) {
+	N = (p3h<<(8+s))|(p3l>>(56-s));
+	a = (p3l<<(8+s))|(p2l>>(56-s));
+      } else if (s == 56) {
+	N = p3l;
+	a = p2l;
+      } else {
+	N = (p3l<<(s-56))|(p2l>>(120-s));
+	a = (p2l<<(s-56))|(p1l>>(120-s));
+      }
+      R = a*0x1p-64;
     }
     R *= PI_OVER_512;
-   
-    double sinpiM, cospiM, sinpiR, cospiR;
+
+    double sinpiM, cospiM, sinpiR, cospiR, temp1, temp2;
     unsigned N2, I;
 
     double sinR = R;
@@ -111,7 +123,7 @@ double rlibm_tanf(float x) {
       sinR = PI_OVER_512 - sinR;
     } 
    
-    if (I & 0x2) s *= -1;
+    if (I & 0x2) sgn ^= 0x8000000000000000;
    
     double sinR2 = sinR * sinR;
 
@@ -119,27 +131,21 @@ double rlibm_tanf(float x) {
     cospiM = cospiMBy512[N2];
     sinpiR, cospiR;
 
-    sinpiR = 0x1.110dd89739b5cp-7;
-    sinpiR *= sinR2;
-    sinpiR += -0x1.55555554ce812p-3;
-    sinpiR *= sinR2;
-    sinpiR += 0x1.ffffffffffffep-1;
-    sinpiR *= sinR;
+   
+    temp1 = fma(sinR2, 0x1.110dd89739b5cp-7, -0x1.55555554ce812p-3);
+    sinpiR = sinR*fma(temp1, sinR2, 0x1.ffffffffffffep-1);
 
-    cospiR = 0x1.5553dd3610b9p-5;
-    cospiR *= sinR2;
-    cospiR += -0x1.ffffffff9c28fp-2; 
-    cospiR *= sinR2;
-    cospiR += 0x1.fffffffffffffp-1; 
-    
-    double sin_x = sinpiM * cospiR + cospiM * sinpiR;
-    sin_x *= s;
+    temp2 = fma(sinR2, 0x1.5553dd3610b9p-5, -0x1.ffffffff9c28fp-2);
+    cospiR = fma(temp2, sinR2, 0x1.fffffffffffffp-1);
+
+
+    double sin_x = fma(sinpiM, cospiR, cospiM * sinpiR);
 
     double cosR = R;
 
     N2 = N & 0xFF;
-    I = (N >> 8) + 1;
-    s = (I & 0x2) ? -1 : 1;
+    I += 1;
+    if (I & 0x2) sgn ^= 0x8000000000000000;
 
     if (I & 1) {
       if (N2 == 0) {
@@ -157,24 +163,19 @@ double rlibm_tanf(float x) {
     }
 
     double cosR2 = cosR * cosR;
+ 
+    temp1 = fma(cosR2, 0x1.110dd89739b5cp-7, -0x1.55555554ce812p-3);
+    sinpiR = cosR*fma(cosR2, temp1, 0x1.ffffffffffffep-1);
+     
+    temp2 = fma(cosR2, 0x1.5553dd3610b9p-5, -0x1.ffffffff9c28fp-2);
+    cospiR = fma(temp2, cosR2, 0x1.fffffffffffffp-1);
 
-    sinpiR = 0x1.110dd89739b5cp-7;
-    sinpiR *= cosR2;
-    sinpiR += -0x1.55555554ce812p-3;
-    sinpiR *= cosR2;
-    sinpiR += 0x1.ffffffffffffep-1;
-    sinpiR *= cosR;
+    double cos_x = fma(sinpiM, sinpiR, cospiM*cospiR);
 
-    cospiR = 0x1.5553dd3610b9p-5;
-    cospiR *= cosR2;
-    cospiR += -0x1.ffffffff9c28fp-2; 
-    cospiR *= cosR2;
-    cospiR += 0x1.fffffffffffffp-1; 
-
-    double cos_x = sinpiM * sinpiR + cospiM * cospiR;
-    cos_x *= s;
-
-    y = sin_x/cos_x;
+    dY.d = sin_x/cos_x;
+    dY.x ^= sgn;
+    return dY.d;
+  } else {
+    return 0.0f/0.0f;
   }
-  return y;
 }
