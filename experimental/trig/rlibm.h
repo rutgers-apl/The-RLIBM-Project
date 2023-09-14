4fa88d37bd3c2e2b25593956685f5449bd98d9a9
Santosh Nagarakatte <sn349@sunny-cove.cs.rutgers.edu>
2023-09-14 15:59:31 -0400

    faster than core-math with core-math testing harness

---
+++ experimental/trig/rlibm.h
@@ -0,0 +1,18 @@
+#ifndef __RLIBM_H__
+#define __RLIBM_H__
+
+typedef unsigned __int128 uint128_t;
+
+typedef union {
+  float f;
+  uint32_t x;
+} float_x;
+
+typedef union {
+  double d;
+  uint64_t x;
+} double_x;
+
+
+
+#endif
