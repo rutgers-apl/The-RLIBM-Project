dd5b2db88233d2d67a28f32792c820810a6e9daf
Santosh Nagarakatte <sn349@sunny-cove.cs.rutgers.edu>
2024-09-19 21:22:08 -0400

    adding sinf.c from the multi-round method

--- libm/sinf.c
+++ libm/sinf.c
@@ -1,8 +1,10 @@
 /*
 MIT License
-Copyright (c) 2023 Sehyeok Park, Mridul Aanjaneya, and Santosh
+
+Copyright (c) 2024 Sehyeok Park, Mridul Aanjaneya, and Santosh
 Nagarakatte, Rutgers Architecture and Programming Languages (RAPL)
 Group
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -24,12 +26,10 @@ SOFTWARE.
 #include "rlibm.h"
 #include "pi.h"
 
-
-#define PI_OVER_256 0x1.921fb54442d18p-7
-
 typedef unsigned __int128 uint128_t;
 
-double rlibm_sinf(float x) {
+double rlibm_sinf(float x){
+  
   float_x fX = {.f=x};
   uint32_t b = fX.x<<1;
   if (b < 0xff000000) {
@@ -39,46 +39,45 @@ double rlibm_sinf(float x) {
     uint64_t m = (fX.x&0x7FFFFF)|1<<23;
     double z, z2;
     if (b < 0x9d000000) {
-      if (__builtin_expect(b == 0x95fbd9c8, 0)) {
-	double y = -0x1.ff6dc18p-1*__builtin_copysign(1.0, x);
-	return y;
-      }
-      if (__builtin_expect(b == 0x995c4a68, 0)) {
-	double y = -0x1.181a858p-6*__builtin_copysign(1.0, x);
-	return y;
-      }
-      if (__builtin_expect(b == 0x9cb0e51c, 0)) {
-	double y = -0x1.851fd78p-14*__builtin_copysign(1.0, x);
-	return y;
-      }
-      if (b < 0x79921fb6) {
+      if (b < 0x79921fb6) { 
 	if (b < 0x73d12ed2) return x*0x1.ffffffffffffep-1;
+	//y=0x1.000000000001p+0 x^(1) + -0x1.55555558a12d3p-3 x^(3) + 0x1.1112f60e1bacep-7 x^(5)
 	z = x;
 	z2 = z*z;
-	double temp = fma(z2, 0x1.11130658ac4e4p-7, -0x1.55555558c07f5p-3);
-	return z*fma(temp, z2, 0x1.0000000000011p+0);
+	double temp = __builtin_fma(z2, 0x1.1112f60e1bacep-7, -0x1.55555558a12d3p-3);
+	return z*__builtin_fma(z2, temp, 0x1.000000000001p+0);
       }
       uint64_t p0 = m*0x441529fc27;
       uint64_t p1 = m*0xa2f9836e4e; p1+=(p0>>40);
       k = (p1>>(33-s));
-      a = p1<<(31+s)|((p0<<24)>>(33-s));
+      a = p1<<(31+s);
+      if (b > 0x8b400000) a |= ((p0<<24)>>(33-s));
+      long sm = a>>63;
+      k -= sm;
+      k += (fX.x>>31)<<8;
+      z = ((a>>10)<<10)*0x1p-64;
+      z2 = z*z;
+      double sinpiK = sinpiMBy256TwoPi[k&511];
+      double cospiK = sinpiMBy256TwoPi[(k+128)&511];
+      double cospiZ;
+      if (__builtin_expect(z == -0x1.a0905cf0d2acp-3, 0)) cospiZ = 0x1.ffff9807c1516p-1;
+      else if (__builtin_expect(z == 0x1.a6d16c710dfb1p-2, 0)) cospiZ = 0x1.fffe51bcc4e2ep-1;
+      else {
+	//-0x1.a0905cf0d2acp-3, 0x1.a6d16c710dfb1p-2
+	//y=0x1.ffffffffffffcp-1 x^(0) + -0x1.3bd3cc9b0252p-14 x^(2) + 0x1.03bc8c4d5592p-30 x^(4) 
+	cospiZ = __builtin_fma(z2, 0x1.03bc8c4d5592p-30, -0x1.3bd3cc9b0252p-14);
+	cospiZ = __builtin_fma(cospiZ, z2, 0x1.ffffffffffffcp-1);
+      }
+      double sinpiZ;
+      if (__builtin_expect(z == 0x1.ef72d3a60f64p-8, 0)) sinpiZ = 0x1.851fd78p-14;
+      else {
+	//0x1.ef72d3a60f64p-8
+	//y=0x1.921fb54442d56p-7 x^(1) + -0x1.4abbce8095fffp-22 x^(3) + 0x1.474321a06f437p-39 x^(5)
+	sinpiZ = __builtin_fma(z2, 0x1.474321a06f437p-39, -0x1.4abbce8095fffp-22);
+	sinpiZ = z*__builtin_fma(sinpiZ, z2, 0x1.921fb54442d56p-7);
+      }
+      return __builtin_fma(sinpiK, cospiZ, cospiK*sinpiZ);
     } else {
-      if (__builtin_expect(b == 0xa4728fec, 0)) {
-	double y = -0x1.24f23b8p-1*__builtin_copysign(1.0, x);
-	return y;
-      }
-      if (__builtin_expect(b == 0xab95f654, 0)) {
-	double y = -0x1.fcf42d8p-1*__builtin_copysign(1.0, x);
-	return y;
-      }
-      if (__builtin_expect(b == 0xcf524856, 0)) {
-	double y = -0x1.ff57018p-1*__builtin_copysign(1.0, x);
-	return y;
-      }
-      if (__builtin_expect(b == 0xe6487e0c, 0)) {
-	double y = 0x1.2875078p-2*__builtin_copysign(1.0, x);
-	return y;
-      }
       static const uint64_t ipi[] = {0xdb6295993c439041, 0xfc2757d1f534ddc0, 0xa2f9836e4e441529};
       uint128_t p1 = (uint128_t)m*ipi[0];
       uint128_t p2 = (uint128_t)m*ipi[1]; p2 += p1>>64;
@@ -94,27 +93,41 @@ double rlibm_sinf(float x) {
 	k = (p3l<<(s-57))|(p2l>>(121-s));
 	a = (p2l<<(s-57))|(p1l>>(121-s));
       }
+      long sm = a>>63;
+      k -= sm;
+      k += (fX.x>>31)<<8;
+      z = ((a>>10)<<10)*0x1p-64;
+      z2 = z*z;
+      double sinpiK = sinpiMBy256TwoPi[k&511];
+      double cospiK = sinpiMBy256TwoPi[(k+128)&511];
+      double cospiZ;
+      if (__builtin_expect(z == -0x1.7882600f9decep-3, 0)) cospiZ = 0x1.ffffab1af4ed6p-1;
+      else if (__builtin_expect(z == -0x1.1b7ecacfe4f68p-3, 0)) cospiZ = 0x1.ffffd016257dp-1;
+      else if (__builtin_expect(z == -0x1.b4f365d6c2c58p-4, 0)) cospiZ = 0x1.ffffe3bff623cp-1;
+      else if (__builtin_expect(z == -0x1.6946952e477ep-7, 0)) cospiZ = 0x1.0000001b907dbp+0;
+      else if (__builtin_expect(z == 0x1.0e8ae19d51ea4p-4, 0)) cospiZ = 0x1.fffff5d5efaccp-1;
+      else if (__builtin_expect(z == 0x1.73b15119dad6fp-2, 0)) cospiZ = 0x1.fffeb23f12e5bp-1;
+      else {	
+	//-0x1.7882600f9decep-3, -0x1.1b7ecacfe4f68p-3, -0x1.b4f365d6c2c58p-4, -0x1.6946952e477ep-7, 0x1.0e8ae19d51ea4p-4, 0x1.73b15119dad6fp-2   
+	//y=0x1.fffffffffffffp-1 x^(0) + -0x1.3bd3cc9ba5bf3p-14 x^(2) + 0x1.03c109dcc1162p-30 x^(4)
+	cospiZ = __builtin_fma(z2, 0x1.03c109dcc1162p-30, -0x1.3bd3cc9ba5bf3p-14);
+	cospiZ = __builtin_fma(cospiZ, z2, 0x1.fffffffffffffp-1);
+      }
+      //y=0x1.921fb54442d21p-7 x^(1) + -0x1.4abbce6e64ccdp-22 x^(3) + 0x1.46dbcfbda25c7p-39 x^(5)
+      double sinpiZ = __builtin_fma(z2, 0x1.46dbcfbda25c7p-39, -0x1.4abbce6e64ccdp-22);
+      sinpiZ = z*__builtin_fma(sinpiZ, z2, 0x1.921fb54442d21p-7);
+      return __builtin_fma(sinpiK, cospiZ, cospiK*sinpiZ);
     }
-    long sm = a>>63;
-    k -= sm;
-    k += (fX.x>>31)<<8;
-    z = a*0x1p-64;
-    z2 = z*z;
-    double sinpiK = sinpiMBy256TwoPi[k&511];
-    double cospiK = sinpiMBy256TwoPi[(k+128)&511];
-    //y=0x1.fffffffffffffp-1 x^(0) + -0x1.3bd3cc9baf37fp-14 x^(2) + 0x1.03c102099750dp-30 x^(4)
-    double cospiZ = fma(z2, 0x1.03c102099750dp-30, -0x1.3bd3cc9baf37fp-14);
-    cospiZ = fma(cospiZ, z2, 0x1.fffffffffffffp-1);
-    double z3 = z2*z;
-    //y=0x1.921fb54442d1cp-7 x^(1) + -0x1.4abbce67633e5p-22 x^(3) + 0x1.46a562b83e363p-39 x^(5)
-    z *= 0x1.921fb54442d1cp-7;
-    double sinpiZ = fma(z2, 0x1.46a562b83e363p-39, -0x1.4abbce67633e5p-22);
-    sinpiZ = fma(sinpiZ, z3, z);
-    return fma(sinpiK, cospiZ, cospiK*sinpiZ);
   } else { 
     return 0.0f/0.0f;
   }
 }
 
+float sinf(float x){
+  return (float) rlibm_sinf(x);
+}
 
 
+float cr_sinf(float x){
+  return (float) rlibm_sinf(x);
+}
